784310871f160b9d8cddaf27508021449e7df344
JuanGt <serrajgt87@gmail.com>
2026-01-03 05:56:06 +0100

    fix(security): se permiten acceso a todos los end points

--- apps/backend/src/main/java/com/incident_manager/security/config/SecurityConfig.java
+++ apps/backend/src/main/java/com/incident_manager/security/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
         );
 
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/login", "/api/groups", "/api/groups/**").permitAll()
+                .requestMatchers("/api/auth/login", "/api/**").permitAll()
                 .anyRequest().authenticated()
         );
 
